11b6bef791fe29261484fed3c04532150e16d41f
Sean Nguyen <nsean321@gmail.com>
2022-02-08 17:37:04 -0800

    setup routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const passport = require('passport')
 
-router.use('/api', require('./userController.js'))
+router.use('/api', require('./scrapeController.js'))
 
 module.exports = router
